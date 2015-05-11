5da6203bdd38a46bd59e9d3efaaa71a0fa3e19bd
José Governo <zegoverno@hotmail.com>
2015-05-11 08:09:09 -0300

    Create trab_pratio_ex4.py

---
+++ trab_pratio_ex4.py
@@ -0,0 +1,21 @@
+def ler():
+    lados =[]
+    for i in range(3):
+        if i != 2:
+            lados.append(int(input("digite um cateto:")))
+        else:
+            lados.append(int(input("digite a hipotenusa")))
+    return lados
+    
+
+def triangulo(lados):
+    if lados[2]**2 == lados[1]**2 + lados[0]**2:
+        return lados[0]*lados[1]/2
+    else:
+        return 0
+
+lados = ler()
+if triangulo(lados) > 0:
+    print('os lados formam um triangulo retangul e sua area e: {0}'.format(triangulo(lados)))
+else:
+    print('os lados {0}, {1} e {2} nao formam um triangulo'.format(lados[0],  lados[1],  lados[2]))
