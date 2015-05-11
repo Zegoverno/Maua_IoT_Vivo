f5a5d47e7af4b11e36cf41247ce53fddfe678408
José Governo <zegoverno@hotmail.com>
2015-05-11 07:58:47 -0300

    Create par_ex2.py

---
+++ par_ex2.py
@@ -0,0 +1,11 @@
+def entrada ():
+	return int(input("Digite um numero: "))
+
+def par (x):
+    if not x % 2 :
+        print('O numero {0} é par'.format(x))
+    else:
+        print('O numero {0} é impar'.format(x))
+    
+x = entrada()
+par(x)
