73be8d34828daaab9d4e5efe4524c075a89ad198
José Governo <zegoverno@hotmail.com>
2015-05-11 08:11:05 -0300

    Create trab_pratio_ex5.py

---
+++ trab_pratio_ex5.py
@@ -0,0 +1,26 @@
+import math
+
+def ler():
+    return input('digite um ponto no formato cartesiano (x,y,z): ').split(',')
+    
+def conv(pto):
+    E = 0.00669454185
+    A = 6378160
+    geo = []
+    h = 0
+    n =1
+    for i in range(5):
+        fi = math.atan(float(pto[2])/p(pto)*(E*n/(n+h)))
+        n = A/(math.sqrt(1-E*(math.sin(fi))**2))
+        h = p(pto)/math.cos(fi)-n
+    geo.append(180*math.atan(float(pto[0])/float(pto[1])/math.pi))
+    geo.append(180*fi/math.pi)
+    geo.append(h)
+    return geo
+    
+def p(pto):
+    return math.sqrt(float(pto[0]) ** 2 + float(pto[1]) ** 2)
+geo = []
+pto = ler()
+geo = conv(pto)
+print('latitude: {0}º, longitude: {1}º, altitude: {2}m'.format(geo[0], geo[1],  geo[2]))
