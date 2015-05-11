88ef73806be4dd9c05da6846d8f33d8ff348a074
José Governo <zegoverno@hotmail.com>
2015-05-11 08:02:02 -0300

    Create media_ex8.py

---
+++ media_ex8.py
@@ -0,0 +1,26 @@
+def media(notas):
+    t = 0
+    for nota in notas:
+        t += nota
+    return t / len(notas)
+
+def maiornota(notas):
+    m = 0
+    for nota in notas:
+        if nota > m:
+            m = nota
+    return m
+    
+notas = []
+nota = 0
+while True:
+    try:
+        nota = float(input("digite uma nota: "))
+    except :
+        print('voce nao digitou um numero')
+        continue
+    if nota < 0:
+        break
+    notas.append(nota)
+print('Media: ',  media(notas))
+print('Maior nota: ', maiornota(notas))
