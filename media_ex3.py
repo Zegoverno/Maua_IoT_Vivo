126c5cd378362c4d8767fd451f0d54170a7e8ab5
José Governo <zegoverno@hotmail.com>
2015-05-11 08:00:07 -0300

    Create media_ex3.py

---
+++ media_ex3.py
@@ -0,0 +1,14 @@
+def media(notas):
+    t = 0
+    for nota in notas:
+        t += nota
+    return t / len(notas)
+    
+notas = []
+nota = 0
+while True:
+    nota = float(input("digite uma nota: "))
+    if nota < 0:
+        break
+    notas.append(nota)
+print('Media: ',  media(notas))
